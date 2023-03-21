7b276b55d2063c4a6161119b5776e07bf062c6bf
ryanmotamen <ryanmotamen@gmail.com>
2023-03-21 16:51:59 -0700

    ignoring node-modules

--- electron-react/src/components/fileReader/FileTree.jsx
+++ electron-react/src/components/fileReader/FileTree.jsx
@@ -9,19 +9,16 @@ const { app } = window.require("@electron/remote");
 const FileTree = () => {
   const [uploadedFiles, setUploadedFiles] = useState([]);
 
-  // const handleFileEvent = (e) => {
-  //   uploadedFiles.push(e.target.files);
-  //   console.log(e.target.files);
-  // }
-
   const generateSubTrees = (fileObj, directoryPath) => {
     // base case: when file is not a directory, return the whole files array;
     for (let i = 0; i < fileObj.length; i++) {
       const file = fileObj[i];
       if (file.directory === true) {
         const subDirectoryPath = pathModule.join(directoryPath, file.name);
-        file.files = 
-        generateSubTrees(fileTreeObject(subDirectoryPath), subDirectoryPath);
+        file.files = generateSubTrees(
+          fileTreeObject(subDirectoryPath),
+          subDirectoryPath
+        );
       }
     }
     return fileObj;
@@ -39,8 +36,11 @@ const FileTree = () => {
 
   const fileTreeObject = (directoryPath) => {
     console.log("DIRECTORY PATH: ", directoryPath);
-    const filesObj = fs
-      .readdirSync(directoryPath)
+    const filesObj = fs.readdirSync(directoryPath);
+    const filteredFileObj = filesObj
+      .filter((file) => {
+        return file !== "node_modules" && file !== ".git";
+      })
       .map((file) => {
         // console.log(file);
         const stats = fs.statSync(pathModule.join(directoryPath, file));
@@ -57,7 +57,7 @@ const FileTree = () => {
         }
         return a.directory ? -1 : 1;
       });
-    return filesObj;
+    return filteredFileObj;
   };
 
   return (
