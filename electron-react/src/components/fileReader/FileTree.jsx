883e21a3686649e85549f32979fca848e09c861d
ryanmotamen <ryanmotamen@gmail.com>
2023-03-21 14:41:51 -0700

    first-level of fileReader
    Co-authored-by: Matthew Fukudome <matthewfukudome@gmail.com>
    Co-authored-by: Derrick Oh <juneparkoh@gmail.com>

--- electron-react/src/components/fileReader/FileTree.jsx
+++ electron-react/src/components/fileReader/FileTree.jsx
@@ -1,8 +1,13 @@
-import { ipcRenderer } from 'electron';
-import React, { useState, useContext } from 'react';
+import { ipcRenderer } from "electron";
+import React, { useState, useContext } from "react";
+
+const fs = window.require("fs");
+const pathModule = window.require("path");
+
+const { app } = window.require("@electron/remote");
 
 const FileTree = () => {
-
+  const [path, setPath] = useState(app.getAppPath());
   const [uploadedFiles, setUploadedFiles] = useState([]);
 
   // const handleFileEvent = (e) => {
@@ -11,22 +16,62 @@ const FileTree = () => {
   // }
 
   const handleOpenFolder = () => {
-    let directory = ipcRenderer.sendSync('OpenFolder');
-    console.log('DIRECTORY HERE!!!: ', directory);
+    const directory = ipcRenderer.sendSync("OpenFolder");
+    console.log("DIRECTORY HERE!!!: ", directory);
     let directoryPath = directory[0];
-    directoryPath = directoryPath.replace(/\\/g, '/');
-    fileTreeObject(directoryPath);
+    directoryPath = directoryPath.replace(/\\/g, "/");
+    const fileObj = fileTreeObject(directoryPath);
+    // console.log(fileObj);
+
+    for (let i = 0; i < fileObj.length; i++){
+      const file = fileObj[i];
+      // console.log(file.name);
+      // console.log(file.directory);
+      if (file.directory === true){
+        // console.log(`${directoryPath}` + '/' + `${file.name}`)
+        // console.log([...fileTreeObject(`${directoryPath}` + '/' + `${file}`)])
+        file.files = [...fileTreeObject(`${directoryPath}` + '/' + `${file.name}`)]
+      }
+    }
+    console.log(fileObj);
   };
 
   const fileTreeObject = (directoryPath) => {
-    console.log('DIRECTORY PATH: ', directoryPath);
-    const filePaths = ipcRenderer.sendSync('ReadDir', directoryPath);
-    console.log('filePaths!!!: ', filePaths);
+    console.log("DIRECTORY PATH: ", directoryPath);
+    const filesObj = fs
+      .readdirSync(directoryPath)
+      .map((file) => {
+        // console.log(file);
+        const stats = fs.statSync(pathModule.join(directoryPath, file));
+        // console.log(stats);
+        return {
+          name: file,
+          directory: stats.isDirectory(),
+          files: []
+        };
+      })
+      .sort((a, b) => {
+        if (a.directory === b.directory) {
+          return a.name.localeCompare(b.name);
+        }
+        return a.directory ? -1 : 1;
+      });
+    return filesObj;
+    // now we have an array of objects, true for folder, false for file
+    // we want to iterate over all of the files in our files array, and if it is a directory, we recursively call our fileTreeObject function to generate a new "files array"
   };
+
+  // const filePaths = ipcRenderer.sendSync('ReadDir', directoryPath);
+  // console.log('filePaths!!!: ', filePaths);
+
+  // const onBack = () => setPath(pathModule.dirname(path));
+  // const onOpen = (folder) => setPath(pathModule.join(path, folder));
 
   return (
     <div>
-      <button className='open-folder-button' onClick={handleOpenFolder}>Open Folder</button>
+      <button className="open-folder-button" onClick={handleOpenFolder}>
+        Open Folder
+      </button>
     </div>
   );
 };
