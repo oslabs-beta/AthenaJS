ed315491d82f34bc8cf9a2832df7c5b02ff4e178
Daniel Chang <dkc213@Daniels-MacBook-Pro.local>
2023-03-20 17:42:35 -0700

    changed the function input Co-authored-by: Derrick Oh <juneparkoh@gmail.com> Co-authored-by: Christopher Long <93620761+cvalong@users.noreply.github.com> Co-authored-by: Daniel Chang <Chang254@mail.chapman.edu> Co-authored-by: Matthew Fukudome <matthewfukudome@gmail.com> Co-authored-by: Ryan Motamen <ryanmotamen@gmail.com>

--- electron-react/src/components/ViewComponent.jsx
+++ electron-react/src/components/ViewComponent.jsx
@@ -1,18 +1,19 @@
 import React, { useContext, useState } from 'react';
 import { DetailsContext } from './context/DetailsContext';
 import useActions from '@/hooks/useActions';
-import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
-import stringifyObject from 'stringify-object'
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
+import stringifyObject from 'stringify-object';
 
 
 const ViewComponent = () => {
-  
   const { compProps, compActions, compHTML } = useContext(DetailsContext);
   const action = useActions(compActions[0]);
   const [styles, setStyles] = compProps;
+  console.log(compActions[0]);
   const string = `() => {
     const [ logs, setLogs ] = React.useState([])
     const actions = ${stringifyObject(compActions[0])}
+    const props = ${stringifyObject(compProps[0])}
     return(  
     <>
       ${compHTML[0]}
