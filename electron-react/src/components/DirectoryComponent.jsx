6469784b6b4f88c03076589ee8c9f3f3a4248878
cvalong <93620761+cvalong@users.noreply.github.com>
2023-03-22 16:14:58 -0700

    adjusted fileExplorer styling Co-authored-by: Derrick Oh <juneparkoh@gmail.com> Co-authored-by: Christopher Long <93620761+cvalong@users.noreply.github.com> Co-authored-by: Daniel Chang <Chang254@mail.chapman.edu> Co-authored-by: Matthew Fukudome <matthewfukudome@gmail.com> Co-authored-by: Ryan Motamen <ryanmotamen@gmail.com>

--- electron-react/src/components/DirectoryComponent.jsx
+++ electron-react/src/components/DirectoryComponent.jsx
@@ -9,21 +9,28 @@ const DirectoryComponent = ({ name, files }) => {
     setOpen(!isOpen);
   };
 
+  // TODO: readability refactor for recursive call
   return (
-    <div className="folder-button">
+    <div className="folder">
       <button className="folder-button" onClick={handleFolderToggle}>
-        {name}
+        <svg className={isOpen ? 'folder-button-icon chevron-down' : 'folder-button-icon'} 
+          alt="chevron" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+          <path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+        </svg>
+        <span className="file-button-text">{name}</span>
       </button>
       {isOpen && (
         <div className="sub-files">
           {files.map((file) => (
-            <div key={file.name}>
-              {file.directory ? (
-                <DirectoryComponent name={file.name} files={file.files} />
-              ) : (
-                <button className="file-button">{file.name}</button>
-              )}
-            </div>
+            // <div >
+            // {
+            file.directory ? 
+              (<DirectoryComponent key={file.name} name={file.name} files={file.files} />) : 
+              (<button key={file.name} className="file-button">
+                <span className="file-button-text">{file.name}</span>
+              </button>)
+            // }
+            // </div>
           ))}
         </div>
       )}
