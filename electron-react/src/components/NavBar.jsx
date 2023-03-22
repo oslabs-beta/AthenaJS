3471402ed69531a6dcc8783eb0733419f6295f0a
Daniel Chang <Chang254@mail.chapman.edu>
2023-03-22 14:36:24 -0700

    mock-fetch capabilities added, moved performance charts Co-authored-by: Derrick Oh <juneparkoh@gmail.com> Co-authored-by: Christopher Long <93620761+cvalong@users.noreply.github.com> Co-authored-by: Daniel Chang <Chang254@mail.chapman.edu> Co-authored-by: Matthew Fukudome <matthewfukudome@gmail.com> Co-authored-by: Ryan Motamen <ryanmotamen@gmail.com>

--- electron-react/src/components/NavBar.jsx
+++ electron-react/src/components/NavBar.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const NavBar = () => {
+const NavBar = ({handleToggleWindow}) => {
   return (
     <nav className='navbar'>
       <ul>
-        <li className="nav-link"><a href='#'>Actions</a></li>
-        <li className="nav-link"><a href='#'>Props</a></li>
+        <li className="nav-link"><a href='#' onClick = {handleToggleWindow.props}>Props</a></li>
+        <li className="nav-link"><a href='#' onClick={handleToggleWindow.performance}>Performance</a></li>
       </ul>
     </nav>
   )
--- electron-react/src/components/ViewComponent.jsx
+++ electron-react/src/components/ViewComponent.jsx
@@ -2,11 +2,14 @@ import React, { Profiler, useContext, useState, useEffect } from 'react';
 import { DetailsContext } from './context/DetailsContext';
 import { PerformanceContext } from './context/PerformanceContext';
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
+import fetchMock from 'fetch-mock';
 import stringifyObject from 'stringify-object';
+import { MockFetchContext } from './context/MockFetchContext';
 
 
 const ViewComponent = () => {
   const { compProps, compActions, compHTML, compState } = useContext(DetailsContext);
+  const { mockServer } = useContext(MockFetchContext);
   const { performanceData } = useContext(PerformanceContext);
   const [ performanceDataArr, setPerformanceDataArr] = performanceData;
   const [ profilerData, setProfilerData ] = useState(null);
@@ -28,13 +31,21 @@ const ViewComponent = () => {
     setPerformanceDataArr([...performanceDataArr, profilerData]);
   };
 
-
-  console.log(profilerData);
+  // fetchMock.mock('*', {data: 'mock data'}, { overwriteRoutes: true });
+  // React.useEffect(() => {
+  // async function getData(){
+  // const res = await fetch('/api/users')
+  // const data = await res.json()
+  // if (res.ok) console.log(data.data)
+  // }
+  // getData()
+  // }, [])
 
   const string = `() => {
     ${compState[0]}
     ${compActions[0]}
     ${compProps[0]}
+    ${mockServer[0]}
     return(  
     <>
       ${compHTML[0]}
@@ -42,7 +53,8 @@ const ViewComponent = () => {
     )
       }`;
 
- 
+  let scope = {};
+  if (mockServer[0]) scope = {fetchMock};
 
   return (
     <div id='navigation-area'>
@@ -50,7 +62,7 @@ const ViewComponent = () => {
       Props: {compProps[0]} <br/>
       State: {compState[0]} <br/>
       Render Time: {profilerData ? profilerData.actualDuration.toFixed(3) + ' ms' : 'N/A'}
-      <LiveProvider code= {string}>
+      <LiveProvider code= {string} scope = {scope}>
         <Profiler id = 'preview-component' onRender={handleProfilerData}>
           <LivePreview />
         </Profiler>
