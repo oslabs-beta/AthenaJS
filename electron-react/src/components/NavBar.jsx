f40ad9467a6406288034aeecf7a5c94987e6ae36
Daniel Chang <Chang254@mail.chapman.edu>
2023-03-23 09:16:30 -0700

    added save component capabilities

--- electron-react/src/components/NavBar.jsx
+++ electron-react/src/components/NavBar.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const NavBar = ({handleToggleWindow}) => {
   return (
     <nav className='navbar'>
       <ul>
-        <li className="nav-link"><a href='#' onClick = {handleToggleWindow.props}>Props</a></li>
+        <li className="nav-link"><a href='#' onClick = {handleToggleWindow.props}>Edit Components</a></li>
+        <li className="nav-link"><a href='#' onClick={handleToggleWindow.savedComps}>Saved Components</a></li>
         <li className="nav-link"><a href='#' onClick={handleToggleWindow.performance}>Performance</a></li>
       </ul>
     </nav>
