c8a23dd9554e1aeb8a62c79fdeb63bdf366968c3
Daniel Chang <Chang254@mail.chapman.edu>
2023-03-22 19:43:17 -0700

    importing common react hooks into react-live scope Co-authored-by: Derrick Oh <juneparkoh@gmail.com> Co-authored-by: Christopher Long <93620761+cvalong@users.noreply.github.com> Co-authored-by: Daniel Chang <Chang254@mail.chapman.edu> Co-authored-by: Matthew Fukudome <matthewfukudome@gmail.com> Co-authored-by: Ryan Motamen <ryanmotamen@gmail.com>

--- electron-react/src/components/context/PerformanceContext.jsx
+++ electron-react/src/components/context/PerformanceContext.jsx
@@ -3,13 +3,13 @@ import React, { useState, createContext } from 'react';
 export const PerformanceContext = createContext();
 
 export const PerformanceProvider = ({ children }) => {
-  const [performanceData, setPerformanceData] = useState([]);
+  const [ performanceData, setPerformanceData ] = useState([]);
   const [ keyCount, setKeyCount ] = useState(0);
     
   return(
     <PerformanceContext.Provider 
       value = {{
-        performanceData: [performanceData, setPerformanceData],
+        performanceData: [performanceData, setPerformanceData ],
         keyCount: [ keyCount, setKeyCount ]
       }}
     >
