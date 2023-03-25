9fe73225ad915dff749e5374dee6d8e8817308ba
Daniel Chang <dkc213@Daniels-MacBook-Pro.local>
2023-03-25 08:49:14 -0700

    change background color for ui + screenshot button

--- electron-react/src/components/UIComps.jsx
+++ electron-react/src/components/UIComps.jsx
@@ -3,15 +3,19 @@ import { useUserCompContext } from '@/hooks/useUserCompContext';
 import { CompUIContext } from './context/CompUIContext';
 const { ipcRenderer } = require('electron');
 
-const UIComps = ({addNode, removeNode}) => {
+const UIComps = ({bg, addNode, removeNode}) => {
   const {components, dispatch} = useUserCompContext();
   const { compsUI } = useContext(CompUIContext);
   const [ compsUIVal, setCompsUIVal ] = compsUI;
+  const [ bgColor, setBgColor ] = bg;
+  const [ bgColorIn, setBgColorIn ] = useState(bgColor);
 
    
   return(
     <div className = 'saved-comp-page'>
       <h2>Saved Components</h2>
+      <input onChange = {(e) => setBgColorIn(e.target.value)} value = {bgColorIn}/>
+      <button onClick = {() => setBgColor(bgColorIn)}>Change BG Color</button>
       {components.length > 0 && components.map( (component) => (
         <div key = {component.name} className = 'saved-comp-container'>
           <button onClick = {() => addNode(component)}>Add {component.name}</button>
