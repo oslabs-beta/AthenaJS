55586ed03e6fa90268ed3538a61585013935dab6
ryanmotamen <ryanmotamen@gmail.com>
2023-03-27 17:23:34 -0700

    more docusaurus styling

--- electron-react/src/components/FileExplorer.jsx
+++ electron-react/src/components/FileExplorer.jsx
@@ -3,6 +3,9 @@ import React, { useState, useContext } from "react";
 import DirectoryComponent from "./DirectoryComponent";
 import { Resizable } from "re-resizable";
 import { DetailsContext } from "./context/DetailsContext";
+// import * as acorn from "acorn";
+import * as walk from "acorn-walk";
+import { parse } from "acorn-loose";
 
 const fs = window.require("fs");
 const pathModule = window.require("path");
@@ -10,15 +13,16 @@ const pathModule = window.require("path");
 /**
  * interface file {name:string, directory: boolean, files: file[] }
  * @returns
-*/
+ */
 const FileExplorer = () => {
   // Contexts from details context
-  const { tempCompProps, tempCompActions, tempCompHTML, tempCompState } = useContext(DetailsContext);
+  const { tempCompProps, tempCompActions, tempCompHTML, tempCompState } =
+    useContext(DetailsContext);
   const [tempCompPropsVal, setTempCompPropsVal] = tempCompProps;
   const [tempCompActionsVal, setTempCompActionsVal] = tempCompActions;
   const [tempCompHTMLVal, setTempCompHTMLVal] = tempCompHTML;
   const [tempCompStateVal, setTempCompStateVal] = tempCompState;
-  
+
   // store htmlArray in state
   const [uploadedFiles, setUploadedFiles] = useState([]);
   // toggle sidebar
@@ -61,7 +65,7 @@ const FileExplorer = () => {
         // fs.statSync is how we get the data on whether a folder is a directory or not.
         const subPath = pathModule.join(directoryPath, file);
         const stats = fs.statSync(subPath);
-        
+
         // console.log(stats);
         return {
           name: file,
@@ -100,44 +104,59 @@ const FileExplorer = () => {
 
   const fileParser = (path) => {
     // console.log(path);
-    //We only really need plaintext here since AthenaJS handles the logic for us, do we even need to parse? 
+    //We only really need plaintext here since AthenaJS handles the logic for us, do we even need to parse?
 
-    // asynchronously read file here passing in the absolute path. 
+    // asynchronously read file here passing in the absolute path.
     // data is a string
-    fs.readFile(path,'utf-8', (err, data) => {
+    fs.readFile(path, "utf-8", (err, data) => {
       //declare variable extension which gets the extension of our file i.e. .jsx
       const extension = pathModule.extname(path).toLowerCase();
+      const ast = parse(data, {ecmaVersion: "latest"});
 
-      //switch statement for different file types? Do we just Need JSX? 
-      //I am going to keep the switch statement here for now, and remove it if we decide to only use JSX
       try {
-        switch(extension) {
-        case '.jsx':
-          // just the jsx return should be passed
-          // regex matches return(<statement>) and also <statement>
-          const returnRegex = /return\s*\((\s*<[\s\S]*?)\)/;
-          // data.match => [return(<statement>), <statement>]
-          const returnStatement = data.match(returnRegex)[1];
-          console.log('JSX File content:', returnStatement);
-          setTempCompHTMLVal(returnStatement);
-          break;
-        case '.json':
-          console.log('JSON File content:', data);
-          //how do we pass along this data into the JSX textarea? 
-          break;
-        case '.js':
-          console.log('JS File content:', data);
-          //how do we pass along this data into the JSX textarea? 
-          break;
-        default: 
-          console.log('File data:', data);
-        }
-      }
-      //handle errors
-      catch(err) {
-        console.log('ERROR: error reading file in DirectoryComponent.jsx:', err);
+        const ast = parse(data, {
+          locations: true,
+          ecmaVersion: "latest",
+          sourceType: "module",
+        });
+      } catch (err) {
+        console.log(
+          "ERROR: error parsing file in DirectoryComponent.jsx:",
+          err
+        );
         return;
       }
+
+      // //switch statement for different file types? Do we just Need JSX?
+      // //I am going to keep the switch statement here for now, and remove it if we decide to only use JSX
+      // try {
+      //   switch(extension) {
+      //   case '.jsx':
+      //     // just the jsx return should be passed
+      //     // regex matches return(<statement>) and also <statement>
+      //     const returnRegex = /return\s*\((\s*<[\s\S]*?)\)/;
+      //     // data.match => [return(<statement>), <statement>]
+      //     const returnStatement = data.match(returnRegex)[1];
+      //     console.log('JSX File content:', returnStatement);
+      //     setTempCompHTMLVal(returnStatement);
+      //     break;
+      //   case '.json':
+      //     console.log('JSON File content:', data);
+      //     //how do we pass along this data into the JSX textarea?
+      //     break;
+      //   case '.js':
+      //     console.log('JS File content:', data);
+      //     //how do we pass along this data into the JSX textarea?
+      //     break;
+      //   default:
+      //     console.log('File data:', data);
+      //   }
+      // }
+      // //handle errors
+      // catch(err) {
+      //   console.log('ERROR: error reading file in DirectoryComponent.jsx:', err);
+      //   return;
+      // }
     });
   };
 
@@ -148,20 +167,23 @@ const FileExplorer = () => {
       const { name, files, path } = file;
       if (file.directory) {
         // if file is a directory
-        // create a directory component passing down name and files and push to htmlarray 
+        // create a directory component passing down name and files and push to htmlarray
         htmlArray.push(
           <DirectoryComponent
-            path = {path}
-            fileParser = {fileParser}
-            name={name} 
-            files={files}></DirectoryComponent>
+            path={path}
+            fileParser={fileParser}
+            name={name}
+            files={files}
+          ></DirectoryComponent>
         );
       } else {
         // else create a button to render that will render on click.
         htmlArray.push(
-          <button 
+          <button
             className="file-button"
-            onClick = {() => {fileParser(path)}}
+            onClick={() => {
+              fileParser(path);
+            }}
           >
             <span className="file-button-text">{name}</span>
           </button>
@@ -177,11 +199,11 @@ const FileExplorer = () => {
         <Resizable
           className={sidebarClass}
           defaultSize={{
-            width: 'auto',
-            height: 'auto',
+            width: "auto",
+            height: "auto",
           }}
-          minWidth={250} 
-          maxWidth={800} 
+          minWidth={250}
+          maxWidth={800}
           enable={{
             top: false,
             right: true,
@@ -197,7 +219,7 @@ const FileExplorer = () => {
             <div className="side-nav">
               <div className="side-nav-buttons-top">
                 <span className="material-icons" onClick={handleToggle}>
-                    arrow_back_ios
+                  arrow_back_ios
                 </span>
               </div>
             </div>
@@ -206,7 +228,7 @@ const FileExplorer = () => {
                 <h2>File Explorer</h2>
                 <span
                   className="material-icons"
-                  id = "open-folder-button"
+                  id="open-folder-button"
                   onClick={() => {
                     handleOpenFolder();
                   }}
@@ -215,8 +237,8 @@ const FileExplorer = () => {
                 </span>
               </div>
               <div className="root-directory">
-                <hr/>
-                <br/>
+                <hr />
+                <br />
                 {/* this is where we render htmlArray */}
                 <div className="root-dir-header">{uploadedFiles}</div>
               </div>
@@ -228,7 +250,7 @@ const FileExplorer = () => {
           <div className="side-nav">
             <div className="side-nav-buttons-top">
               <span className="material-icons" onClick={handleToggle}>
-              arrow_forward_ios
+                arrow_forward_ios
               </span>
             </div>
           </div>
