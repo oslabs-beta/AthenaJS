5e744680b7d25f2efa19ddbeb18337386697dabc
mattfookoo <matthewfukudome@gmail.com>
2023-03-29 16:32:29 -0700

    added buttons to remove components from the ReactFlow UI without having to use the UIComps component
    Co-authored-by: Derrick Oh <juneparkoh@gmail.com>
    Co-authored-by: Christopher Long <93620761+cvalong@users.noreply.github.com>
    Co-authored-by: Daniel Chang <Chang254@mail.chapman.edu>
    Co-authored-by: Ryan Motamen <ryanmotamen@gmail.com>

--- electron-react/src/components/ReactFlowComp.jsx
+++ electron-react/src/components/ReactFlowComp.jsx
@@ -6,26 +6,39 @@ import styled from 'styled-components';
 import '@reactflow/node-resizer/dist/style.css';
 
 //Custom React Flow Component to render our saved components on the react flow UI board.
-const ReactFlowComp = ({ data, selected }) => {
-  const { component } = data;
-  const scope = {useState, useEffect, useRef, useMemo, styled, fetchMock};
+const ReactFlowComp = ({ data: {component, removeNode}, selected, }) => {
+  //I commented out the destructuring of data and destructured within the function params -Matt
+  // { component } = data;
+  const scope = { useState, useEffect, useRef, useMemo, styled, fetchMock, component };
+
+  //Feel free to change the styling of the remove button but using inline styling here just in case we decide to inject CSS into the component
+  const buttonStyle = {
+    position: 'absolute',
+    top: '-25px',
+    right: '-20px',
+    cursor: 'pointer',
+    padding: '0px',
+    color: 'red',
+    fontWeight: 'bold',
+    fontSize: '2rem',
+  };
+  
+
+  //add a function to remove the node from the react flow UI when the remove button is clicked
+  const handleRemoveNode = () => {
+    // console.log('Removing component', component);
+    removeNode(component);
+  };
 
   return (
     <>
+      {/* adding a button here to remove the node on click */}
+      <div style={buttonStyle} onClick={handleRemoveNode}>
+        <span>&times;</span>
+      </div>
       <NodeResizer color="#ff0071" isVisible={selected} minWidth={100} minHeight={30} />
-      <LiveProvider code = {
-        `() => {
-            ${component.mockServer}
-            ${component.body}
-            return(
-            <>
-                ${component.jsx}
-            </>
-            )
-        }`
-      }
-      scope = {scope}>
-        <LivePreview/>
+      <LiveProvider code={`() => {${component.mockServer}${component.body}return(${component.jsx})}`} scope={scope}>
+        <LivePreview />
       </LiveProvider>
     </>
   );
