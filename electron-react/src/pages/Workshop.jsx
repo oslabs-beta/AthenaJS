53f7414a0a80f1155000cc8e0a887e713d173016
Daniel Chang <dkc213@Daniels-MacBook-Pro.local>
2023-04-07 15:05:56 -0700

    Styling changes Co-authored-by: Derrick Oh <juneparkoh@gmail.com> Co-authored-by: Christopher Long <93620761+cvalong@users.noreply.github.com> Co-authored-by: Daniel Chang <Chang254@mail.chapman.edu> Co-authored-by: Matthew Fukudome <matthewfukudome@gmail.com> Co-authored-by: Ryan Motamen <ryanmotamen@gmail.com>

--- electron-react/src/pages/Workshop.jsx
+++ electron-react/src/pages/Workshop.jsx
@@ -1,15 +1,33 @@
 import React, { useContext } from 'react';
 import NavigationContainer from '@/components/Navigation/NavigationContainer';
 import ViewComponent from '@/components/WorkshopMain/ViewComponent';
-import AthenaLogoSvg from '@/components/framerMotion/AthenaLogo';
+import { Resizable } from 're-resizable';
 
 const Workshop = () => {
   return(
     <div id = 'workshop-page'>
-      <AthenaLogoSvg/>
-      {/* <AthenaSvg/> */}
       <ViewComponent />
+      <Resizable
+      className="navigation-area-resizable"
+      defaultSize={{
+        width: '100%',
+        height: 'auto',
+      }}
+      minHeight={'30vh'} 
+      maxHeight={'70vh'} 
+      enable={{
+        top: true,
+        right: false,
+        bottom: false,
+        left: false,
+        topRight: false,
+        bottomRight: false,
+        bottomLeft: false,
+        topLeft: false,
+      }}
+    >
       <NavigationContainer />
+    </Resizable>
     </div>
   );
 
