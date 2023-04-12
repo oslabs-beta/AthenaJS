00e9b9f58fb76e6f447bc558a6be6814f7bb2047
cvalong <93620761+cvalong@users.noreply.github.com>
2023-04-12 14:03:06 -0700

    scrubbed console.logs Co-authored-by: Derrick Oh <juneparkoh@gmail.com> Co-authored-by: Christopher Long <93620761+cvalong@users.noreply.github.com> Co-authored-by: Daniel Chang <Chang254@mail.chapman.edu> Co-authored-by: Matthew Fukudome <matthewfukudome@gmail.com> Co-authored-by: Ryan Motamen <ryanmotamen@gmail.com>

--- electron-react/src/components/Navigation/NavigationContainer.tsx
+++ electron-react/src/components/Navigation/NavigationContainer.tsx
@@ -1,16 +1,16 @@
-import React, { useContext, useState } from 'react';
-import PropsWindow from '../WorkshopMain/PropsWindow';
-import NavBar from './NavBar';
-import PerformanceCharts from '../WorkshopMain/PerformanceCharts';
-import SavedComps from '../WorkshopMain/SavedComps';
-import { NavigationContainerProps, handleToggleWindow } from './NavTypes';
-
+import React, { useContext, useState } from "react";
+import PropsWindow from "../WorkshopMain/PropsWindow";
+import NavBar from "./NavBar";
+import PerformanceCharts from "../WorkshopMain/PerformanceCharts";
+import SavedComps from "../WorkshopMain/SavedComps";
+import { NavigationContainerProps, handleToggleWindow } from "./NavTypes";
 
 const NavigationContainer: React.FC = () => {
   const [showPropsWindow, setShowPropsWindow] = useState<boolean>(true);
-  const [showPerformanceCharts, setShowPerformanceCharts] = useState<boolean>(false);
+  const [showPerformanceCharts, setShowPerformanceCharts] =
+    useState<boolean>(false);
   const [showSavedComps, setShowSavedComps] = useState<boolean>(false);
-  // console.log(`logs are in NavigationContainer: ${logs}`);
+
   const handleToggleWindow: handleToggleWindow = {
     props: (e) => {
       setShowPropsWindow(true);
@@ -26,19 +26,19 @@ const NavigationContainer: React.FC = () => {
       setShowSavedComps(true);
       setShowPerformanceCharts(false);
       setShowPropsWindow(false);
-    }
+    },
   };
 
   return (
     <div id="navigation-container">
       <NavBar handleToggleWindow={handleToggleWindow} />
-      <div id='navigation-area'>
+      <div id="navigation-area">
         {showPropsWindow && <PropsWindow />}
         {showSavedComps && <SavedComps />}
         {showPerformanceCharts && <PerformanceCharts />}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default NavigationContainer;
